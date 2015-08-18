bae7e4b6fca6297dea7741d94088206f32bcd0d2
Steven <ChrisPC\Steven>
2015-08-18 08:21:50 -0500

    Add address 2 BBLe in core

--- IntranetPortal/IntranetPortal.Core/Utility.vb
+++ IntranetPortal/IntranetPortal.Core/Utility.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Reflection
 Imports System.ComponentModel
+Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json.Linq
 
 Public Class Utility
     Public Shared Function SaveChangesObj(oldObj As Object, newObj As Object) As Object
@@ -64,5 +67,47 @@ Public Class Utility
         Else
             Return EnumConstant.ToString()
         End If
+    End Function
+
+    'Change address 2 BBLE
+    Function Address2BBLE(houseNumber As String, street As String, borough As String) As String
+        Dim baseURL = "https://api.cityofnewyork.us/geoclient/v1/address.json?app_id=be97fb56&app_key=b51823efd58f25775df3b2956a7b2bef"
+        baseURL = baseURL & "&houseNumber=" & houseNumber
+        baseURL = baseURL & "&street=" & street
+        baseURL = baseURL & "&borough=" & borough
+        '"https://api.cityofnewyork.us/geoclient/v1/address.json?app_id=9cd0a15f&app_key=54dc84bcaca9ff4877da771750033275&houseNumber=433&street=EAST%208TH%20STREET&borough=BROOKLYN"
+        Dim request As WebRequest = WebRequest.Create(baseURL)
+        ' If required by the server, set the credentials.
+        request.Credentials = CredentialCache.DefaultCredentials
+
+        Try
+            request.GetResponse()
+        Catch ex As Exception
+            Return "Get Error " + houseNumber + "," + street + "," + borough
+        End Try
+        ' Get the response. 
+        Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+        ' Display the status.
+        Console.WriteLine(response.StatusDescription)
+        ' Get the stream containing content returned by the server. 
+        Dim dataStream As Stream = response.GetResponseStream()
+        ' Open the stream using a StreamReader for easy access. 
+        Dim reader As New StreamReader(dataStream)
+        ' Read the content. 
+        Dim reslut As String = reader.ReadToEnd()
+        Dim values = JObject.Parse(reslut)
+        Dim info = values.Item("address")
+        Dim Erray = ""
+        If (info IsNot Nothing) Then
+            Dim bbl = info.Item("bbl")
+            If (bbl IsNot Nothing) Then
+                Return bbl
+            Else
+                Erray = "Get Error " + If(info.Item("message") IsNot Nothing, info.Item("message").ToString, "")
+            End If
+        Else
+            Erray = "Get Error " + houseNumber + "," + street + "," + borough
+        End If
+        Return Erray
     End Function
 End Class
