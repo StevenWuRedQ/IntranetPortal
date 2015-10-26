3e953c9bedd79d1614a2da53d5917d37ee022ee8
steven2 <ChrisPC\steven2>
2015-10-26 13:51:23 -0500

    add forms

---
+++ LoopFormResart/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ProcessGrid = new System.Windows.Forms.DataGridView();
+            this.CheckLoop = new System.Windows.Forms.CheckBox();
+            this.Restart = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CheckStatus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ProcessGrid
+            // 
+            this.ProcessGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProcessGrid.Location = new System.Drawing.Point(1, 84);
+            this.ProcessGrid.Name = "ProcessGrid";
+            this.ProcessGrid.Size = new System.Drawing.Size(811, 150);
+            this.ProcessGrid.TabIndex = 1;
+            // 
+            // CheckLoop
+            // 
+            this.CheckLoop.AutoSize = true;
+            this.CheckLoop.Location = new System.Drawing.Point(131, 16);
+            this.CheckLoop.Name = "CheckLoop";
+            this.CheckLoop.Size = new System.Drawing.Size(92, 17);
+            this.CheckLoop.TabIndex = 2;
+            this.CheckLoop.Text = "Cheking Loop";
+            this.CheckLoop.UseVisualStyleBackColor = true;
+            this.CheckLoop.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(242, 10);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 3;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 41);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 519);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.CheckLoop);
+            this.Controls.Add(this.ProcessGrid);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView ProcessGrid;
+        private System.Windows.Forms.CheckBox CheckLoop;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ LoopFormResart/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showProcess();
+        }
+        private void showProcess()
+        {
+           
+            ProcessGrid.DataSource = GetDroneMain().Select( clsProcess=> new { Name = clsProcess.ProcessName, Responding = clsProcess.Responding } );
+        }
+        private List< Process> GetDroneMain()
+        {
+            var name = "TestComponent_VB";
+            var process_list = new List<Process>();
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+
+                if (clsProcess.ProcessName.Contains(name))
+                {
+
+
+                    //if the process is found to be running then we
+                    //return a true
+                    process_list.Add(clsProcess);
+                    //process_list.Add();
+
+                }
+            }
+            return process_list;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckLoop.Text = CheckLoop.Checked ? "" : "";
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public delegate void StatusChangeDelegate(String status);
+        private StatusChangeDelegate statusChangeListener;
+        private int DroneResponseCount = 0;
+        private string MAIN_DRONE_PATH = "C:\\VS2013 Development\\TestComponent_VB\\TestComponent_VB\\bin\\Debug\\TestComponent_VB.exe";
+        private bool is_resarting = false;
+       
+
+        public  void OnStart()
+        {
+          
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000 * 60;//*5*6; // 5 mintues 
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.ScanDroneMain);
+            timer.Start();
+
+
+            //ChangeSatus("Start");
+        }
+
+        public void ScanDroneMain(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            try
+            {
+                log.Debug("====================Scan Start=======================");
+                using (WCFAPI.WCFMacrosClient client = new WCFAPI.WCFMacrosClient())
+                {
+                    try
+                    {
+
+                        log.Debug("Get dorne waiting count : (" + client.Requests_Waiting() + ") Reset respose cont = 0");
+                        DroneResponseCount = 0;
+                    }
+                    catch
+                    {
+                        DroneResponseCount++;
+                        log.Debug("increse Drone Response Count to: (" + DroneResponseCount + ")");
+
+                    }
+
+                }
+
+                if (DroneResponseCount >= 3)
+                {
+                    log.Debug("Server not response in 3 times starting resart it");
+
+                    ResartDrone();
+
+                }
+                log.Debug("====================Scan End=======================");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+            }
+
+
+        }
+      
+        private Process GetDroneMainProcess()
+        {
+            var drones = GetDroneMain();
+            var mainDrone = drones.Count > 0 ? drones[0] : null;
+            return mainDrone;
+        }
+        private void ResartDrone()
+        {
+            /* lock the restarting process*/
+            if (is_resarting == true)
+            {
+                log.Debug("is resarting the main drone ingrone this time");
+                return;
+            }
+            is_resarting = true;
+            log.Debug("************start resart main drone***************");
+            var drones = GetDroneMain();
+            var mainDrone = drones.Count > 0 ? drones[0] : null;
+
+            if (mainDrone == null)
+            {
+                log.Debug("Can not find drone run in system then run it!");
+                mainDrone = Process.Start(MAIN_DRONE_PATH);
+                mainDrone.WaitForInputIdle();
+            }
+            else
+            {
+                log.Debug("Start ran drone process");
+                if (GetDroneMainProcess() != null)
+                {
+                    mainDrone.Kill();
+                    mainDrone.WaitForExit();
+
+                }
+
+                log.Debug("Closed main drone completed! Start resart drone process");
+                mainDrone = Process.Start(MAIN_DRONE_PATH);
+                mainDrone.WaitForInputIdle();
+            }
+            log.Debug("resart Drone Completed");
+            log.Debug("***********end resart main drone****************");
+            is_resarting = false;
+        }
+
+        public void SetStatusChangeListener(StatusChangeDelegate del)
+        {
+            statusChangeListener = del;
+        }
+        protected  void OnStop()
+        {
+            log.Debug("On Stop");
+            ChangeSatus("Stop");
+        }
+
+        private void ChangeSatus(String status)
+        {
+            if (statusChangeListener != null)
+            {
+                statusChangeListener(status);
+            }
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            OnStart();
+        }
+    }
+}
