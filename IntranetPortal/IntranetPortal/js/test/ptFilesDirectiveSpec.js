0a59d933fab183196b38e57eec0ef9ce5fec8a8b
Shaopeng Zhang <stephenz@myidealprop.com>
2015-12-11 11:45:10 -0500

    Modify Readme and create wiki

--- IntranetPortal/IntranetPortal/js/test/ptFilesDirectiveSpec.js
+++ IntranetPortal/IntranetPortal/js/test/ptFilesDirectiveSpec.js
@@ -1,6 +1,7 @@
 ﻿describe('ptFiles', function () {
 
     var scope, celem;
+    var jsonstr = '[{"name":"image_(14).jpeg","folder":"one","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(14).jpeg","thumb":"6308"},{"name":"image_(13).jpeg","folder":"two","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(13).jpeg","thumb":"6304"},{"name":"image_(15).jpeg","folder":"three","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(15).jpeg","thumb":"6314"}]';
 
     beforeEach(function () {
         module("PortalApp");
@@ -9,7 +10,7 @@
             var template = $templateCache.get('js/templates/ptfiles.html');
             $templateCache.put('/js/templates/ptfiles.html', template);
             scope = $rootScope.$new();
-                    scope.files = angular.fromJson('[{"name":"image_(14).jpeg","folder":"one","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(14).jpeg","thumb":"6308"},{"name":"image_(13).jpeg","folder":"two","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(13).jpeg","thumb":"6304"},{"name":"image_(15).jpeg","folder":"three","uploadTime":"2015-09-30T19:59:02.131Z","path":"/Shared%20Documents/1004490003/Construction/Photos_PMPhotos/dfex/image_(15).jpeg","thumb":"6314"}]');
+            scope.files = angular.fromJson(jsonstr);
             var elem = angular.element('<pt-files file-bble="00000000" file-id="testUpload" file-model="files" folder-enable="true" base-folder="testFolder"></pt-files>');
 
             celem = $compile(elem)(scope);
@@ -38,10 +39,6 @@
         var folderDropdown = celem.find('ul.dropdown-menu');
         dump(folderDropdown.scope().fileModel);
         expect(folderDropdown.find('li').length).toBe(3+3);
-    })
-
-    it("must fails", function () {
-        expect(true).toBe(false);
     })
 
 })
--- README.md
+++ README.md
@@ -1,29 +1,21 @@
-# README #
+# IntranetPortal #
 
-This README would normally document whatever steps are necessary to get your application up and running.
+CRM system for My Ideal Property Agents.
 
-### What is this repository for? ###
+### Info 
 
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+The main project folder is IntranetPortal.  
 
-### How do I get set up? ###
+Under IntranetPortal:
 
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
+- IntranetPortal: WebForm based webapp for user interface
+- IntranetPortal.Data: Entity Framework Data Model 
+- IntranetPortal.Core: Some Uitility Helper functions And Class
+- IntranetPortal.RuleEngine: Business rules defined here.
+- RuleEngineManagement: Windows Form to start the engine
+- RuleEngineService: Installer to install Rule Engine Service
+- UnitTestProject: UnitTest For IntranetPortal Project
 
-### Contribution guidelines ###
+### Get Start
 
-* Writing tests
-* Code review
-* Other guidelines
-
-### Who do I talk to? ###
-
-* Repo owner or admin
-* Other community or team contact
+See our project [wiki](https://bitbucket.org/myidealkings/intranetportal/wiki/Home)
