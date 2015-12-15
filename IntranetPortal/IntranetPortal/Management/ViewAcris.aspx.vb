8825cbc9a7cf1881ac8aea8438efd2486a6e4ff4
Steven Wu <stevenw@myidealprop.com>
2015-12-15 14:29:19 -0500

    Change View Acris

--- IntranetPortal/IntranetPortal/Management/ViewAcris.aspx.vb
+++ IntranetPortal/IntranetPortal/Management/ViewAcris.aspx.vb
@@ -7,24 +7,27 @@ Public Class ViewAcris
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Dim bble = Request.QueryString("bble")
         If (Not String.IsNullOrEmpty(bble)) Then
-            Using wc = New WebClient()
-                Dim json = wc.DownloadString("https://api.cityofnewyork.us/geoclient/v1/bin.json?app_id=be97fb56&app_key=b51823efd58f25775df3b2956a7b2bef&bin=" & bble)
-                If (Not String.IsNullOrEmpty(json)) Then
-                    Try
-                        Dim lead = JObject.Parse(json)
-                        Dim boro = lead.SelectToken("bin.bblBoroughCode")
-                        If (boro IsNot Nothing) Then
-                            Dim block = lead.SelectToken("bin.bblTaxBlock")
-                            Dim lot = lead.SelectToken("bin.bblTaxLot")
-                            Dim url = "http://a836-acris.nyc.gov/bblsearch/bblsearch.asp?max_rows=99&borough=" & boro.ToString & "&block=" & CInt(block.ToString) & "&lot=" & CInt(lot.ToString)
-                            Response.Redirect(url)
-                        End If
-                    Catch ex As Exception
 
-                    End Try
-                End If
-            End Using
+            Dim boro = bble.Substring(0, 1)
+                Dim Block = bble.Substring(1, 5)
+                Dim Lot = bble.Substring(6, bble.Length - 6)
+                'Dim json = wc.DownloadString("https://api.cityofnewyork.us/geoclient/v1/bin.json?app_id=be97fb56&app_key=b51823efd58f25775df3b2956a7b2bef&bble=" & bble)
+                'If (Not String.IsNullOrEmpty(json)) Then
+                '    Try
+                '        Dim lead = JObject.Parse(json)
+                '        Dim boro = lead.SelectToken("bin.bblBoroughCode")
+                '        If (boro IsNot Nothing) Then
+                '            Dim block = lead.SelectToken("bin.bblTaxBlock")
+                '            Dim lot = lead.SelectToken("bin.bblTaxLot")
+                Dim url = "http://a836-acris.nyc.gov/bblsearch/bblsearch.asp?max_rows=99&borough=" & boro.ToString & "&block=" & CInt(block.ToString) & "&lot=" & CInt(lot.ToString)
+                            Response.Redirect(url)
+                    '    End If
+            'Catch ex As Exception
+
+            'End Try
         End If
+
+
     End Sub
 
 End Class
