6da49756bbf152041106ab073e1b475526c1481d
chris <chrisy@myidealprop.com>
2015-12-18 11:25:46 -0500

    Override getleaddatasource

--- IntranetPortal/IntranetPortal/CodeFolder/LeadManage.vb
+++ IntranetPortal/IntranetPortal/CodeFolder/LeadManage.vb
@@ -1,4 +1,5 @@
-﻿''' <summary>
+﻿Imports IntranetPortal
+''' <summary>
 ''' Handle lead related action
 ''' </summary>
 Public Class LeadManage
@@ -13,8 +14,18 @@ Public Class LeadManage
 
     Public Sub New(actityLog As Boolean)
         Me.LogCategory = LeadsActivityLog.LogCategory.SalesAgent
-        Me.LogCategoryFilter = {LeadsActivityLog.LogCategory.SalesAgent}
+        Me.LogCategoryFilter = {LeadsActivityLog.LogCategory.SalesAgent, LeadsActivityLog.LogCategory.Status}
     End Sub
+
+    Public Overrides Function LogDataSource(bble As String) As List(Of LeadsActivityLog)
+        Dim categories = Me.LogCategoryFilter.ToList
+        Dim subCategories = {LeadsActivityLog.LogCategory.Approval, LeadsActivityLog.LogCategory.Approved, LeadsActivityLog.LogCategory.Appointment,
+                             LeadsActivityLog.LogCategory.Declined, LeadsActivityLog.LogCategory.DoorknockTask, LeadsActivityLog.LogCategory.PublicUpdate}
+        categories.AddRange(subCategories)
+
+        Return LeadsActivityLog.GetLeadsActivityLogs(bble, categories.Select(Function(a) a.ToString).ToArray)
+    End Function
+
 #End Region
 
 End Class
