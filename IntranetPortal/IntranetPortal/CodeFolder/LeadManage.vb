ad2d2b04b3a19931eab36049a7c63100ce7dfe5e
chris <chrisy@myidealprop.com>
2015-12-18 10:36:34 -0500

    add leads log permission

---
+++ IntranetPortal/IntranetPortal/CodeFolder/LeadManage.vb
@@ -0,0 +1,20 @@
+﻿''' <summary>
+''' Handle lead related action
+''' </summary>
+Public Class LeadManage
+    Inherits ActivityManageBase
+
+
+#Region "Activitylog Manage"
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(actityLog As Boolean)
+        Me.LogCategory = LeadsActivityLog.LogCategory.SalesAgent
+        Me.LogCategoryFilter = {LeadsActivityLog.LogCategory.SalesAgent}
+    End Sub
+#End Region
+
+End Class
