10cb4e9dc9e905b5e0ca84b749ff23526db07321
Steven Wu <stevenw@myidealprop.com>
2016-03-01 16:44:49 -0500

    Change secondary action view

--- IntranetPortal/IntranetPortal/LegalUI/LegalSecondaryActionTab.ascx.vb
+++ IntranetPortal/IntranetPortal/LegalUI/LegalSecondaryActionTab.ascx.vb
@@ -1,4 +1,6 @@
-﻿Public Class LegalSecondaryActionTab
+﻿Imports IntranetPortal.Data
+
+Public Class LegalSecondaryActionTab
     Inherits System.Web.UI.UserControl
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -11,7 +13,7 @@
             Dim wli = WorkflowService.LoadTaskProcess(sn)
             Dim bble = wli.ProcessInstance.DataFields("BBLE").ToString
             wli.Finish()
-           
+
             'update legal case status
             Dim lc = IntranetPortal.Data.LegalCase.GetCase(bble)
             lc.Status = IntranetPortal.Data.LegalCaseStatus.Closed
@@ -22,5 +24,15 @@
             Response.End()
         End If
     End Sub
+    Public Shared Function GetTypeByTag(tag As Integer) As String
+        Dim sencondaryTypes = DataStatu.LoadDataStatus(LegalCase.SecondaryTypeStatusCategory)
+        If (sencondaryTypes IsNot Nothing) Then
+            Dim stat = sencondaryTypes.Where(Function(t) t.Status = tag).FirstOrDefault
+            If (stat IsNot Nothing) Then
+                Return stat.Name
+            End If
+        End If
 
+        Return "Not found"
+    End Function
 End Class
