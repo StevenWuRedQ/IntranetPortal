08d6f16abf737f123734899cb8c31ce307639320
Steven Wu <stevenw@myidealprop.com>
2016-03-14 09:26:32 -0400

    add Action list ctrl

---
+++ IntranetPortal/IntranetPortal/AuctionLeads/AuctionListCtrl.ascx.vb
@@ -0,0 +1,11 @@
+﻿Imports IntranetPortal
+
+Public Class AuctionListCtrl
+    Inherits System.Web.UI.UserControl
+    ''test
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+
+End Class
