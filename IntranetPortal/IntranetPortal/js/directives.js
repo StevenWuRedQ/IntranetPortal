2ed73b4af6410cc7fdf10d6975838c0de7fccf78
Steven Wu <stevenw@myidealprop.com>
2016-08-11 12:35:25 -0400

    add to do in yes no control

--- IntranetPortal/IntranetPortal/js/directives.js
+++ IntranetPortal/IntranetPortal/js/directives.js
@@ -717,6 +717,33 @@
         }
 
     }])
+    /**
+     * @author steven
+     * @date 8/11/2016
+     * @todo
+     *  the pre condition should will in the control which need
+     *  be controller and cleared by yes or no selected.
+     * 
+     * @param {'ngModel'} ) {
+        return {
+            require
+     * @param {function (scope} link
+     * @param element
+     * @param attrs
+     * @param ngModelController) {
+                scope.$watch(attrs.preCondition
+     * @param function (newVal
+     * @param oldVal) {
+                    if (!newVal)
+                        eval('scope.' + attrs.ngModel + '=null');                  
+                }
+     * @param true);
+
+            }
+        };
+    }
+     * @returns {type} 
+     */
     .directive('preCondition', function () {
         return {
             require: 'ngModel',           
