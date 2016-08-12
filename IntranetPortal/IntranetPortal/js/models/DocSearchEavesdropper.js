dfacfcd3809d71e3ff1b5e2fc17b69ad5d64aa26
Steven Wu <stevenw@myidealprop.com>
2016-08-12 11:56:17 -0400

    fix the eaves dropper for show the new version and the old version

--- IntranetPortal/IntranetPortal/js/models/DocSearchEavesdropper.js
+++ IntranetPortal/IntranetPortal/js/models/DocSearchEavesdropper.js
@@ -6,7 +6,8 @@ angular.module('PortalApp')
      * @date   8/12/2016
      * @returns class of Eaves dropper 
      */
-    .factory('DocSearchEavesdropper', function () {
+
+    .factory('DocSearchEavesdropper', function (DocNewVersionConfig) {
         var docSearchEavesdropper = function () {
 
         }
@@ -15,25 +16,82 @@ angular.module('PortalApp')
          * @author steven
          * @date   8/12/2016
          * @description:
-         *  set evaesdrapper
+         *  set evaesdrapper public function very import
          */
-        docSearchEavesdropper.prototype.setEavesdropper(_eavesdropper)
+        docSearchEavesdropper.prototype.setEavesdropper = function(_eavesdropper,revFunc)
         {
             this.eavesDropper = _eavesdropper;
+            this.endorseCheckFuncs();
+            this._registerCheckFuncs();
+            this.endorse(revFunc);
         }
 
         /**
          * @author steven
          * @date   8/12/2016
          * @description:
-         *  endorse evaesdrapper
+         *  endorse evaesdropper
          */
-        docSearchEavesdropper.prototype.endorse = function()
-        {
-            if (!this.eavesDropper)
+        docSearchEavesdropper.prototype.endorse = function (revFunc) {
+            if (!this.eavesDropper) {
                 console.error('unable to eavesdropper it not set yet');
+            }
 
-            this.eavesDropper.endorseCheckDate = "";
+            if (typeof reFunc != 'function')
+                console.error("set rev function have been set up");
+
+            this.endorseCheckFuncs();
+            
+            this.revFunc = revFunc;
+        }
+
+        /**
+         * @author steven
+         * @date   8/12/2016
+         * @description: 
+         *  public function very import start function
+         */
+        docSearchEavesdropper.prototype.start2Eaves = function () {
+            this.endorseCheckFuncs();
+
+            if (this.endorseCheckDate(DocNewVersionConfig.getInstance().date)
+                || this.endorseCheckVersion())
+            {
+                this.revFunc(true);
+            }
+        }
+        /**
+         * @author steven
+         * @date   8/12/2016
+         * @description:
+         *  check all necessary check function registered
+         */
+        docSearchEavesdropper.prototype.endorseCheckFuncs = function () {
+            var eaves = this.eavesDropper;
+           
+            if ( typeof eaves.endorseCheckDate != 'function')
+            {
+                console.error("eavesDropper functions is not null");
+            }
+
+            if (typeof eaves.endorseCheckVersion != 'function') {
+                console.error("eavesDropper functions is not null");
+            }
+           
+        }
+        /**
+         * @author steven
+         * @date   8/12/2016
+         * @description:
+         *  registerd check functions
+         */
+        docSearchEavesdropper.prototype._registerCheckFuncs = function()
+        {
+            var eaves = this.eavesDropper;
+            this.endorseCheckFuncs();
+
+            this.endorseCheckDate = eaves.endorseCheckDate;
+            this.endorseCheckVersion = eaves.endorseCheckVersion;
         }
 
         /**
