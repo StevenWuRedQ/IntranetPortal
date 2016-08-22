d44a2895240051db3933c7c56d467656057e2f1a
Stephen Zhang <stephenz@myidealprop.com>
2016-08-22 18:23:05 -0400

    merge steven code

--- IntranetPortal/IntranetPortal/js/build/intranetportal.js
+++ IntranetPortal/IntranetPortal/js/build/intranetportal.js
@@ -3897,9 +3897,8 @@ angular.module('PortalApp')
 
         $scope.versionController = new DocSearchEavesdropper()
         $scope.versionController.setEavesdropper($scope, $scope.GoToNewVersion);
-       
-        $scope.multipleValidated = function (base, boolKey, arraykey)
-        {
+
+        $scope.multipleValidated = function (base, boolKey, arraykey) {
             var boolVal = base[boolKey];
             var arrayVal = base[arraykey];
             /**
@@ -4004,9 +4003,8 @@ angular.module('PortalApp')
                 return true;
             }
 
-            if (!$scope.DivError.passValidate())
-            {
-               
+            if (!$scope.DivError.passValidate()) {
+
                 return false;
             }
 
@@ -4037,7 +4035,7 @@ angular.module('PortalApp')
                 "has_Vacate_Order_Vacate_Order",
                 "has_ECB_Tickets_ECB_Tickets",
                 "has_ECB_on_Name_ECB_on_Name_other_known_address",
-               
+
                 /**
                  * @author Steven
                  * @date   8/19/2016
@@ -4046,7 +4044,7 @@ angular.module('PortalApp')
                  * git commit bde6b6d tax search
                  * add validated to new version doc search at least one item add 
                  * when select yes control grid
-                 */ 
+                 */
                 // under are one to multiple//
                 "Has_Other_Mortgage",
                 "Has_Other_Liens",
@@ -4075,7 +4073,7 @@ angular.module('PortalApp')
 
                 for (var j = 0; j < checkedAttrs.length; j++) {
                     var f1 = checkedAttrs[j];
-                    if( ( fields[f1[0]] === true && !Array.isArray(fields[f1[1]]) ) || (fields[f1[0]] === true && fields[f1[1]].length === 0)){
+                    if ((fields[f1[0]] === true && !Array.isArray(fields[f1[1]])) || (fields[f1[0]] === true && fields[f1[1]].length === 0)) {
                         errormsg = errormsg + f1[1] + " has checked but have no value.<br>";
                     }
                 }
@@ -4086,13 +4084,9 @@ angular.module('PortalApp')
 
         }
 
-
-
-
         $scope.SearchComplete = function (isSave) {
 
-            if (!$scope.newVersionValidate())
-            {
+            if (!$scope.newVersionValidate()) {
                 var msg = $scope.DivError.getMessage();
 
                 AngularRoot.alert(msg[0]);
@@ -4161,6 +4155,22 @@ angular.module('PortalApp')
             //    }
 
             //});
+        }
+
+        $scope.EXCLUSIVE_FIELD = ['DocSearch.LeadResearch.fha', 'DocSearch.LeadResearch.fannie', 'DocSearch.LeadResearch.Freddie_Mac_'];
+        // under the $watch, in the listener function
+        // this.eq is the evaled value
+        // this.exp is the watched field
+        for (var i = 0; i < $scope.EXCLUSIVE_FIELD.length; i++) {
+            $scope.$watch($scope.EXCLUSIVE_FIELD[i], function (nv, ov) {
+                if (nv) {
+                    var rest_exclusive_filed = _.without($scope.EXCLUSIVE_FIELD, this.exp);
+                    for (var j = 0; j < rest_exclusive_filed.length; j++) {
+                        if ($scope.$eval(rest_exclusive_filed[j])) $scope.$eval(rest_exclusive_filed[j] + '=false');
+                    }
+                }
+
+            })
         }
     });
 /* global LegalShowAll */
