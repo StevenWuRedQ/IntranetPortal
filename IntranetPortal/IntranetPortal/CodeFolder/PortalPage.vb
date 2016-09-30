e1e81ed273cec18e7cfa7dadb8d7fe72ff1555d9
chris <chrisy@myidealprop.com>
2016-09-30 10:50:17 -0400

    update leads search permission

--- IntranetPortal/IntranetPortal/CodeFolder/PortalPage.vb
+++ IntranetPortal/IntranetPortal/CodeFolder/PortalPage.vb
@@ -1,6 +1,12 @@
 ﻿Public Class PortalPage
     Inherits Page
 
+    Public ReadOnly Property UserName As String
+        Get
+            Return Page.User.Identity.Name
+        End Get
+    End Property
+
     Public ReadOnly Property CurrentUser As Employee
         Get
             Return Employee.GetInstance(User.Identity.Name)
