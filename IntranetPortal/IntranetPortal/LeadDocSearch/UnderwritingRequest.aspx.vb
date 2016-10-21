9ec1e23c1e7884e8d13685d7e5f282c4f01994d6
chris <chrisy@myidealprop.com>
2016-10-21 17:30:08 -0400

    update menu permission

--- IntranetPortal/IntranetPortal/LeadDocSearch/UnderwritingRequest.aspx.vb
+++ IntranetPortal/IntranetPortal/LeadDocSearch/UnderwritingRequest.aspx.vb
@@ -1,5 +1,5 @@
 ﻿Public Class UnderwritingRequest
-    Inherits System.Web.UI.Page
+    Inherits LeadsBasePage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
