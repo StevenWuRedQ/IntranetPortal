aa3490924a4260ccac9a31b8b37d642e30919b4e
Stephen Zhang <stephenz@myidealprop.com>
2016-11-30 14:56:52 -0500

    api completed need more testing

--- IntranetPortal/IntranetPortal.Data/DialerService/DialerService.vb
+++ IntranetPortal/IntranetPortal.Data/DialerService/DialerService.vb
@@ -2,8 +2,8 @@
 Imports System.IO
 Imports System.Net
 Imports System.Net.Http
-Imports System.Web.Script.Serialization
 Imports CsvHelper
+Imports CsvHelper.Configuration
 Imports Newtonsoft.Json
 Imports Newtonsoft.Json.Linq
 
@@ -23,26 +23,34 @@ Public Class DialerService
     End Sub
 
     Public Async Function InitialExport(listid As String) As Task(Of Boolean)
-        Dim url = "/api/v2/outbound/contactlists/{0}/export"
-        url = String.Format(url, listid)
-        Dim content = New StringContent("", Text.Encoding.UTF8, "application/json")
-        Dim response = Await APIClient.PostAsync(url, content)
-        If response.IsSuccessStatusCode Then
-            Return True
-        End If
-        Return False
+        Try
+            Dim url = "/api/v2/outbound/contactlists/{0}/export"
+            url = String.Format(url, listid)
+            Dim content = New StringContent("", Text.Encoding.UTF8, "application/json")
+            Dim response = Await APIClient.PostAsync(url, content)
+            If response.IsSuccessStatusCode Then
+                Return True
+            End If
+            Return False
+        Catch ex As Exception
+            Return False
+        End Try
     End Function
 
     Public Async Function GetExportURL(ListId As String) As Task(Of ContactListExportURLItem)
         Dim item As ContactListExportURLItem = Nothing
-        Dim path = "/api/v2/outbound/contactlists/{0}/export"
-        path = String.Format(path, ListId)
-        Dim response = Await APIClient.GetAsync(path)
-        If (response.IsSuccessStatusCode) Then
-            Dim responsestr = Await response.Content.ReadAsStringAsync()
-            item = JsonConvert.DeserializeObject(Of ContactListExportURLItem)(responsestr)
-        End If
-        Return item
+        Try
+            Dim path = "/api/v2/outbound/contactlists/{0}/export"
+            path = String.Format(path, ListId)
+            Dim response = Await APIClient.GetAsync(path)
+            If (response.IsSuccessStatusCode) Then
+                Dim responsestr = Await response.Content.ReadAsStringAsync()
+                item = JsonConvert.DeserializeObject(Of ContactListExportURLItem)(responsestr)
+            End If
+            Return item
+        Catch ex As Exception
+            Return item
+        End Try
     End Function
 
     Public Async Function ExportList(ListId As String) As Task(Of List(Of DialerContact))
@@ -102,7 +110,8 @@ Public Class DialerService
     End Function
 
     Public Async Function AddContactList(listName As String) As Task(Of JObject)
-        Dim template = <![CDATA[Hello{
+        Try
+            Dim template = <![CDATA[{
             "name": "{0}",
             "version": 1,
             "columnNames": [
@@ -318,15 +327,19 @@ Public Class DialerService
             "previewModeColumnName": "",
             "previewModeAcceptedValues": []
                 }]]>.Value
-        Dim data = String.Format(template, listName)
-        Dim content = New StringContent(data, Text.Encoding.UTF8, "application/json")
-        Dim url = "/api/v2/outbound/contactlists"
-        Dim response = Await APIClient.PostAsync(url, content)
-        If response.IsSuccessStatusCode Then
-            Dim responsestr = Await response.Content.ReadAsStringAsync()
-            Return JsonConvert.DeserializeObject(responsestr)
-        End If
-        Return Nothing
+            Dim data = template.Replace("{0}", listName)
+            Dim content = New StringContent(data, Text.Encoding.UTF8, "application/json")
+            Dim url = "/api/v2/outbound/contactlists"
+            Dim response = Await APIClient.PostAsync(url, content)
+            If response.IsSuccessStatusCode Then
+                Dim responsestr = Await response.Content.ReadAsStringAsync()
+                Return JsonConvert.DeserializeObject(responsestr)
+            End If
+            Return Nothing
+        Catch ex As Exception
+
+        End Try
+
     End Function
 
     Public Async Function DeleteContactList(listid As String) As Task(Of Boolean)
@@ -387,9 +400,13 @@ Public Class DialerService
 
     Public Async Function UpdateContactInList(contact As DialerContact) As Task(Of JObject)
         Dim json = Await GetContactFromList(contact.ContactListId, contact.inin_outbound_id)
-        Dim newdata = ConvertContactToJson(contact)
-        json.Property("data").Remove()
-        json.Add("data", newdata)
+        Dim newdata = ConvertContactToJson(contact, True)
+        json("data") = newdata
+        json.Property("id").Remove()
+        json.Property("selfUri").Remove()
+        If json("callRecords") IsNot Nothing Then
+            json.Property("callRecords").Remove()
+        End If
 
         Dim url = "/api/v2/outbound/contactlists/{0}/contacts/{1}"
         url = String.Format(url, contact.ContactListId, contact.inin_outbound_id)
@@ -403,72 +420,141 @@ Public Class DialerService
 
     End Function
 
-    Public Function ParseCSV(response As Stream) As List(Of DialerContact)
-        If response IsNot Nothing Then
-            Dim stream = New StreamReader(response, Text.UTF8Encoding.UTF8)
-            Dim csv = New CsvReader(stream)
-            csv.Configuration.HasHeaderRecord = True
+    Public Function ParseCSV(stream As Stream) As List(Of DialerContact)
+        If stream IsNot Nothing Then
+            Dim csv = New CsvReader(New StreamReader(stream, Text.UTF8Encoding.UTF8))
             csv.Configuration.QuoteAllFields = True
+
+            Dim contactMap = New DefaultCsvClassMap(Of DialerContact)()
+            For Each prop In GetType(DialerContact).GetProperties
+                Dim newMap = New CsvPropertyMap(prop)
+                newMap.Name(prop.Name.Replace("_", "-"))
+                contactMap.PropertyMaps.Add(newMap)
+            Next
+
+            csv.Configuration.RegisterClassMap(contactMap)
             Dim records = csv.GetRecords(Of DialerContact)().ToList()
         End If
     End Function
 
-    Public Function ConvertContactToJson(contact As DialerContact) As JObject
+    Public Function ConvertContactToJson(contact As DialerContact, Optional isTry As Boolean = False) As JObject
         Dim dataObject = New JObject()
-        dataObject.Add("BBLE", contact.BBLE)
-        dataObject.Add("Leads", contact.Leads)
-        dataObject.Add("Address", contact.Address)
-        dataObject.Add("Comments", contact.Comments)
-        dataObject.Add("Owner", contact.Owner)
-        dataObject.Add("CoOwner", contact.CoOwner)
+        If Not isTry Then
+            dataObject.Add("BBLE", contact.BBLE)
+            dataObject.Add("Leads", contact.Leads)
+            dataObject.Add("Address", contact.Address)
+            dataObject.Add("Comments", contact.Comments)
+            dataObject.Add("Owner", contact.Owner)
+            dataObject.Add("CoOwner", contact.CoOwner)
 
-        dataObject.Add("OwnerPhone1", contact.OwnerPhone1)
-        dataObject.Add("CoOwnerPhone1", contact.CoOwnerPhone1)
-        dataObject.Add("OwnerPhone2", contact.OwnerPhone2)
-        dataObject.Add("CoOwnerPhone2", contact.CoOwnerPhone2)
-        dataObject.Add("OwnerPhone3", contact.OwnerPhone3)
-        dataObject.Add("CoOwnerPhone3", contact.CoOwnerPhone3)
-        dataObject.Add("OwnerPhone4", contact.OwnerPhone4)
-        dataObject.Add("CoOwnerPhone4", contact.CoOwnerPhone4)
-        dataObject.Add("OwnerPhone5", contact.OwnerPhone5)
-        dataObject.Add("CoOwnerPhone5", contact.CoOwnerPhone5)
-        dataObject.Add("OwnerPhone6", contact.OwnerPhone6)
-        dataObject.Add("CoOwnerPhone6", contact.CoOwnerPhone6)
-        dataObject.Add("OwnerPhone7", contact.OwnerPhone7)
-        dataObject.Add("CoOwnerPhone7", contact.CoOwnerPhone7)
-        dataObject.Add("OwnerPhone8", contact.OwnerPhone8)
-        dataObject.Add("CoOwnerPhone8", contact.CoOwnerPhone8)
-        dataObject.Add("OwnerPhone9", contact.OwnerPhone9)
-        dataObject.Add("CoOwnerPhone9", contact.CoOwnerPhone9)
-        dataObject.Add("OwnerPhone10", contact.OwnerPhone10)
-        dataObject.Add("CoOwnerPhone10", contact.CoOwnerPhone10)
-        dataObject.Add("OwnerPhone11", contact.OwnerPhone11)
-        dataObject.Add("CoOwnerPhone11", contact.CoOwnerPhone11)
-        dataObject.Add("OwnerPhone12", contact.OwnerPhone12)
-        dataObject.Add("CoOwnerPhone12", contact.CoOwnerPhone12)
-        dataObject.Add("OwnerPhone13", contact.OwnerPhone13)
-        dataObject.Add("CoOwnerPhone13", contact.CoOwnerPhone13)
-        dataObject.Add("OwnerPhone14", contact.OwnerPhone14)
-        dataObject.Add("CoOwnerPhone14", contact.CoOwnerPhone14)
-        dataObject.Add("OwnerPhone15", contact.OwnerPhone15)
-        dataObject.Add("CoOwnerPhone15", contact.CoOwnerPhone15)
-        dataObject.Add("OwnerPhone16", contact.OwnerPhone16)
-        dataObject.Add("CoOwnerPhone16", contact.CoOwnerPhone16)
-        dataObject.Add("OwnerPhone17", contact.OwnerPhone17)
-        dataObject.Add("CoOwnerPhone17", contact.CoOwnerPhone17)
-        dataObject.Add("OwnerPhone18", contact.OwnerPhone18)
-        dataObject.Add("CoOwnerPhone18", contact.CoOwnerPhone18)
-        dataObject.Add("OwnerPhone19", contact.OwnerPhone19)
-        dataObject.Add("CoOwnerPhone19", contact.CoOwnerPhone19)
-        dataObject.Add("OwnerPhone20", contact.OwnerPhone20)
-        dataObject.Add("CoOwnerPhone20", contact.CoOwnerPhone20)
+            dataObject.Add("OwnerPhone1", contact.OwnerPhone1)
+            dataObject.Add("CoOwnerPhone1", contact.CoOwnerPhone1)
+            dataObject.Add("OwnerPhone2", contact.OwnerPhone2)
+            dataObject.Add("CoOwnerPhone2", contact.CoOwnerPhone2)
+            dataObject.Add("OwnerPhone3", contact.OwnerPhone3)
+            dataObject.Add("CoOwnerPhone3", contact.CoOwnerPhone3)
+            dataObject.Add("OwnerPhone4", contact.OwnerPhone4)
+            dataObject.Add("CoOwnerPhone4", contact.CoOwnerPhone4)
+            dataObject.Add("OwnerPhone5", contact.OwnerPhone5)
+            dataObject.Add("CoOwnerPhone5", contact.CoOwnerPhone5)
+            dataObject.Add("OwnerPhone6", contact.OwnerPhone6)
+            dataObject.Add("CoOwnerPhone6", contact.CoOwnerPhone6)
+            dataObject.Add("OwnerPhone7", contact.OwnerPhone7)
+            dataObject.Add("CoOwnerPhone7", contact.CoOwnerPhone7)
+            dataObject.Add("OwnerPhone8", contact.OwnerPhone8)
+            dataObject.Add("CoOwnerPhone8", contact.CoOwnerPhone8)
+            dataObject.Add("OwnerPhone9", contact.OwnerPhone9)
+            dataObject.Add("CoOwnerPhone9", contact.CoOwnerPhone9)
+            dataObject.Add("OwnerPhone10", contact.OwnerPhone10)
+            dataObject.Add("CoOwnerPhone10", contact.CoOwnerPhone10)
+            dataObject.Add("OwnerPhone11", contact.OwnerPhone11)
+            dataObject.Add("CoOwnerPhone11", contact.CoOwnerPhone11)
+            dataObject.Add("OwnerPhone12", contact.OwnerPhone12)
+            dataObject.Add("CoOwnerPhone12", contact.CoOwnerPhone12)
+            dataObject.Add("OwnerPhone13", contact.OwnerPhone13)
+            dataObject.Add("CoOwnerPhone13", contact.CoOwnerPhone13)
+            dataObject.Add("OwnerPhone14", contact.OwnerPhone14)
+            dataObject.Add("CoOwnerPhone14", contact.CoOwnerPhone14)
+            dataObject.Add("OwnerPhone15", contact.OwnerPhone15)
+            dataObject.Add("CoOwnerPhone15", contact.CoOwnerPhone15)
+            dataObject.Add("OwnerPhone16", contact.OwnerPhone16)
+            dataObject.Add("CoOwnerPhone16", contact.CoOwnerPhone16)
+            dataObject.Add("OwnerPhone17", contact.OwnerPhone17)
+            dataObject.Add("CoOwnerPhone17", contact.CoOwnerPhone17)
+            dataObject.Add("OwnerPhone18", contact.OwnerPhone18)
+            dataObject.Add("CoOwnerPhone18", contact.CoOwnerPhone18)
+            dataObject.Add("OwnerPhone19", contact.OwnerPhone19)
+            dataObject.Add("CoOwnerPhone19", contact.CoOwnerPhone19)
+            dataObject.Add("OwnerPhone20", contact.OwnerPhone20)
+            dataObject.Add("CoOwnerPhone20", contact.CoOwnerPhone20)
+        Else
+            TryAdd(dataObject, "BBLE", contact.BBLE)
+            TryAdd(dataObject, "Leads", contact.Leads)
+            TryAdd(dataObject, "Address", contact.Address)
+            TryAdd(dataObject, "Comments", contact.Comments)
+            TryAdd(dataObject, "Owner", contact.Owner)
+            TryAdd(dataObject, "CoOwner", contact.CoOwner)
+
+            TryAdd(dataObject, "OwnerPhone1", contact.OwnerPhone1)
+            TryAdd(dataObject, "CoOwnerPhone1", contact.CoOwnerPhone1)
+            TryAdd(dataObject, "OwnerPhone2", contact.OwnerPhone2)
+            TryAdd(dataObject, "CoOwnerPhone2", contact.CoOwnerPhone2)
+            TryAdd(dataObject, "OwnerPhone3", contact.OwnerPhone3)
+            TryAdd(dataObject, "CoOwnerPhone3", contact.CoOwnerPhone3)
+            TryAdd(dataObject, "OwnerPhone4", contact.OwnerPhone4)
+            TryAdd(dataObject, "CoOwnerPhone4", contact.CoOwnerPhone4)
+            TryAdd(dataObject, "OwnerPhone5", contact.OwnerPhone5)
+            TryAdd(dataObject, "CoOwnerPhone5", contact.CoOwnerPhone5)
+            TryAdd(dataObject, "OwnerPhone6", contact.OwnerPhone6)
+            TryAdd(dataObject, "CoOwnerPhone6", contact.CoOwnerPhone6)
+            TryAdd(dataObject, "OwnerPhone7", contact.OwnerPhone7)
+            TryAdd(dataObject, "CoOwnerPhone7", contact.CoOwnerPhone7)
+            TryAdd(dataObject, "OwnerPhone8", contact.OwnerPhone8)
+            TryAdd(dataObject, "CoOwnerPhone8", contact.CoOwnerPhone8)
+            TryAdd(dataObject, "OwnerPhone9", contact.OwnerPhone9)
+            TryAdd(dataObject, "CoOwnerPhone9", contact.CoOwnerPhone9)
+            TryAdd(dataObject, "OwnerPhone10", contact.OwnerPhone10)
+            TryAdd(dataObject, "CoOwnerPhone10", contact.CoOwnerPhone10)
+            TryAdd(dataObject, "OwnerPhone11", contact.OwnerPhone11)
+            TryAdd(dataObject, "CoOwnerPhone11", contact.CoOwnerPhone11)
+            TryAdd(dataObject, "OwnerPhone12", contact.OwnerPhone12)
+            TryAdd(dataObject, "CoOwnerPhone12", contact.CoOwnerPhone12)
+            TryAdd(dataObject, "OwnerPhone13", contact.OwnerPhone13)
+            TryAdd(dataObject, "CoOwnerPhone13", contact.CoOwnerPhone13)
+            TryAdd(dataObject, "OwnerPhone14", contact.OwnerPhone14)
+            TryAdd(dataObject, "CoOwnerPhone14", contact.CoOwnerPhone14)
+            TryAdd(dataObject, "OwnerPhone15", contact.OwnerPhone15)
+            TryAdd(dataObject, "CoOwnerPhone15", contact.CoOwnerPhone15)
+            TryAdd(dataObject, "OwnerPhone16", contact.OwnerPhone16)
+            TryAdd(dataObject, "CoOwnerPhone16", contact.CoOwnerPhone16)
+            TryAdd(dataObject, "OwnerPhone17", contact.OwnerPhone17)
+            TryAdd(dataObject, "CoOwnerPhone17", contact.CoOwnerPhone17)
+            TryAdd(dataObject, "OwnerPhone18", contact.OwnerPhone18)
+            TryAdd(dataObject, "CoOwnerPhone18", contact.CoOwnerPhone18)
+            TryAdd(dataObject, "OwnerPhone19", contact.OwnerPhone19)
+            TryAdd(dataObject, "CoOwnerPhone19", contact.CoOwnerPhone19)
+            TryAdd(dataObject, "OwnerPhone20", contact.OwnerPhone20)
+            TryAdd(dataObject, "CoOwnerPhone20", contact.CoOwnerPhone20)
+        End If
+
+
 
         Return dataObject
     End Function
 
+    Public Sub TryAdd(target As JObject, propertyName As String, value As JToken)
+        If String.IsNullOrEmpty(value.ToString) Then
+            Return
+        Else
+            target.Add(propertyName, value)
+        End If
+    End Sub
 End Class
+
+
 
 Public Class ContactListExportURLItem
     Public Property uri As String
     Public Property exportTimestamp As DateTime
 End Class
+
