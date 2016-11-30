6b8981cb2206f6180cc55ff09e49a6727db62da8
Stephen Zhang <stephenz@myidealprop.com>
2016-11-30 16:20:43 -0500

    update json.net

---
+++ IntranetPortal/IntranetPortal.Data/DialerService/Class1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Class1
+    Public Sub test()
+        Dim server = New ININ.PureCloudApi.Api.OutboundApi
+        Dim contact = server.GetContactlistsContactlistIdContactsContactId("1", "1")
+        contact.Callable = True
+    End Sub
+End Class
