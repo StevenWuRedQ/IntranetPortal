11f205532aca70ce79a4bc5585b978967ecbc751
chris <chrisy@myidealprop.com>
2016-12-05 10:25:04 -0500

    add listingtype to homeownerphone

--- IntranetPortal/IntranetPortal/DataModel/PortalEntityModel.Designer.vb
+++ IntranetPortal/IntranetPortal/DataModel/PortalEntityModel.Designer.vb
@@ -1,4 +1,4 @@
-﻿' T4 code generation is enabled for model 'D:\TasksWebApplication\IntranetPortal\IntranetPortal\DataModel\PortalEntityModel.edmx'. 
+﻿' T4 code generation is enabled for model 'G:\Working Folder\WebApp\IntranetPortalGit\IntranetPortal\IntranetPortal\DataModel\PortalEntityModel.edmx'. 
 ' To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 ' property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 ' is open in the designer.
